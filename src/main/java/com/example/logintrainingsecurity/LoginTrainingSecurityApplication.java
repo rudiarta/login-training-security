d1cbe62fc644577af552f26c05faec28930d9c2c
rudiarta <rudiarta@icube.us>
2019-05-31 13:56:51 +0700

    first init

---
+++ src/main/java/com/example/logintrainingsecurity/LoginTrainingSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.logintrainingsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginTrainingSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginTrainingSecurityApplication.class, args);
+	}
+
+}
