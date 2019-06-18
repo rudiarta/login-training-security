618f2d9637a64025507ed0a9dd70754b090ac682
rudiarta <rudiarta@icube.us>
2019-06-18 17:09:36 +0700

    fix login password

--- src/main/java/com/example/logintrainingsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/example/logintrainingsecurity/config/SecurityConfiguration.java
@@ -50,7 +50,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
             @Override
             public boolean matches(CharSequence charSequence, String s) {
-                return true;
+                return encode(charSequence).equals(s);
             }
         };
     }
